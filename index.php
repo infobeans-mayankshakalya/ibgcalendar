fb6252b8cd27b034407cbfca6acf234c49e3ae89
Vipin Sharma <vipin.sharma@infobeans.com>
2016-06-09 16:27:50 +0530

    log

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php 
-if(mail("vipin.sharma@infobeans.com","My subject",'worked'))
-echo 1;
-else
-echo 0;
 if($_POST){
 // the message
 $msg = json_encode($_POST);
 // send email
-mail("vipin.sharma@infobeans.com","My subject",$msg);
+$myfile = fopen("push_notification.txt", "w") or die("Unable to open file!");
+$txt = "POST DATA:\n";
+fwrite($myfile, $txt.$msg);
+fclose($myfile);
 }
 
 ?>
