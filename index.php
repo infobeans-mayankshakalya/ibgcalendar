42dcf761a3f2da7007d13a5de7866ac52073191c
Vipin Sharma <vipin.sharma@infobeans.com>
2016-06-09 19:49:09 +0530

    index

--- index.php
+++ index.php
@@ -14,10 +14,10 @@ function pg_connection_string_from_database_url() {
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
 //pg_query($pg_conn, "CREATE TABLE postdata( id integer PRIMARY KEY NOT NULL, post_data text NOT NULL);");
-pg_query($pg_conn, "INSERT INTO postdata(id, post_data) values (1,'12345')");
+pg_query($pg_conn, "INSERT INTO postdata(id, post_data) values (2,'1234567')");
 $result = pg_query($pg_conn, "SELECT * FROM postdata;");
 var_dump($result);
-echo 'deployed 12345';
+echo 'deployed 1234567';
 print "<pre>\n";
 if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
