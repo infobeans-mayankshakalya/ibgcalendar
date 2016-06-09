65465a9ffff99e3097ce59c41d70f39fb0c13521
Vipin Sharma <vipin.sharma@infobeans.com>
2016-06-09 17:21:22 +0530

    update permission

--- index.php
+++ index.php
@@ -8,6 +8,14 @@ $myfile = fopen("push_notification.txt", "w") or die("Unable to open file!");
 $txt = "POST DATA:\n";
 fwrite($myfile, $txt.$msg);
 fclose($myfile);
+}else{
+        chmod('push_notification.txt', '0777');
+// the message
+$msg = "no data found\nTime: ".date('H:i A',time());
+// send email
+$myfile = fopen("push_notification.txt", "w") or die("Unable to open file!");
+fwrite($myfile, $msg);
+fclose($myfile);
 }
 
 ?>
