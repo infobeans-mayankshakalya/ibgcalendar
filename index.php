c35289795bd1c673d3c668144674e3c6a97d33f3
Vipin Sharma <vipin.sharma@infobeans.com>
2016-06-09 19:00:41 +0530

    pg ddatabase

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
 <?php 
+
+# This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+    $user = "tcpqujlrgdxpqx";
+    $pass = "b_wZht5KuCU1WjqZx0mLX7XkOK";
+    $host = "ec2-54-163-238-222.compute-1.amazonaws.com";
+  extract(parse_url($_ENV["postgres://tcpqujlrgdxpqx:b_wZht5KuCU1WjqZx0mLX7XkOK@ec2-54-163-238-222.compute-1.amazonaws.com:5432/d5ejpv5kc8s1je"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+# Here we establish the connection. Yes, that's all.
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+# Now let's use the connection for something silly just to prove it works:
+$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+print "<pre>\n";
+if (!pg_num_rows($result)) {
+  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+} else {
+  print "Tables in your database:\n";
+  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+}
+print "\n";
+
+
+die;
+
+
+
 require 'PHPMailer/PHPMailerAutoload.php';
 
 $mail = new PHPMailer;
